043046bc17e90a73e1a179ca20e48dfc1c1667ac
pushpanavik <pushpanavik17@gmail.com>
2018-06-16 18:31:04 +0530

    some changes commited

--- src/main/java/com/bridgeit/service/UserServiceImpl.java
+++ src/main/java/com/bridgeit/service/UserServiceImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bridgeit.dao.IUserDao;
+import com.bridgeit.exception.Tokens;
 import com.bridgeit.interceptor.Validation;
 import com.bridgeit.model.User;
 
@@ -18,9 +20,29 @@ public class UserServiceImpl implements IUserService {
 	@Autowired
 	IUserDao userDao;
 	
+	@Autowired
+	BCryptPasswordEncoder encoder;
+	
 	@Transactional
 	public String addUser(User user, HttpServletRequest req) {
-		String getDetails=Validation.userValidation(user.getUsername(),user.getEmail(), user.getPassword());
+		String getDetails=Validation.userValidation(user.getUsername(),user.getEmail(),user.getPassword());
+		
+		if(getDetails!=null)
+		{
+			return getDetails;
+		}
+		String URL=req.getRequestURI();
+		String generataHash=encoder.encode(user.getPassword());
+		user.setPassword(generataHash);
+		
+		int id = userDao.addUser(user);
+		System.out.println("My  primary id is:" +id);
+		
+		String token = Tokens.generateToken(id);
+		System.out.println("my Token.... "+token);
+
+		int id1 = Tokens.getId(token);
+		System.out.println("My id via JWT token..."+ id);
 		return null;
 	}
 
